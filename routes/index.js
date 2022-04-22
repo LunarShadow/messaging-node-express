05736bf1c2b9d0290ae0a4567ccfe1e525ff0c72
Nelson Shu <ns@Nelsons-MBP.lan>
2022-04-22 12:59:06 -0500

    home login UI and Message UI

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res, next) => { 
-  res.render('index', { title: "Test", status: 'Log out'})
+  res.render('index', { title: "Home Page", status: 'Log out', style:'index.css'})
+});
+
+router.get('/message', (req, res, next) => { 
+  res.render('message', { title: "Message Page", status: '', style: 'message.css'})
 });
 
 module.exports = router
